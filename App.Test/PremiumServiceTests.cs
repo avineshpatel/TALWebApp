c84e6bce8019b9d47fc08544e765480c12ad7503
avineshpatel <islandboy01@gmail.com>
2020-10-25 00:38:17 +1100

    Added initial PremiumService TestCase(s)

---
+++ App.Test/PremiumServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using App.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Test
+{
+    [TestFixture]
+    public class PremiumServiceTests
+    {
+        private IPremiumService _premiumService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _premiumService = new PremiumService();
+        }
+
+        [TestCase(-1, 1.25, 40)]
+        [TestCase(0, 1.25, 40)]
+        [TestCase(100000, 0.25, -1)]
+        public void GetPremiumArgumentExceptionTest(int sumInsured, decimal ratingFactor, int age)
+        {
+            Assert.That(() => _premiumService.GetPremium(sumInsured, ratingFactor, age), Throws.TypeOf<ArgumentException>());
+        }
+
+     
+
+    }
+}
