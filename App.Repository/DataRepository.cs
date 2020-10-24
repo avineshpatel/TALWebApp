7379cd2897e8399f17b02b2981c6b522dbba8c0a
avineshpatel <islandboy01@gmail.com>
2020-10-24 15:19:50 +1100

    Added simple repository interface and implementation for the required data.

---
+++ App.Repository/DataRepository.cs
@@ -0,0 +1,41 @@
+﻿using App.Data;
+using App.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace App.Repository
+{
+    public class DataRepository : IDataRespository
+    {
+        public OccupationRating GetOccupationRating(string rating)
+        {
+            return GetOccupationRatings().Where(r => r.Rating == rating).FirstOrDefault();
+        }
+
+        public List<OccupationRating> GetOccupationRatings()
+        {
+            return new List<OccupationRating>()
+            {
+                new OccupationRating () { Rating = "Professional", Factor = 1.0M },
+                new OccupationRating () { Rating = "White Collar", Factor = 1.25M },
+                new OccupationRating () { Rating = "Light Manual", Factor = 1.50M },
+                new OccupationRating () { Rating = "Heavy Manual", Factor = 1.75M }
+            };
+        }
+
+        public List<Occupation> GetOccupations()
+        {
+            return new List<Occupation>()
+            {
+                new Occupation() { Name = "Cleaner", Rating = "Light Manual" },
+                new Occupation() { Name = "Doctor", Rating = "Professional" },
+                new Occupation() { Name = "Author", Rating = "White Collar" },
+                new Occupation() { Name = "Farmer", Rating = "Heavy Manual" },
+                new Occupation() { Name = "Mechanic", Rating = "Heavy Manual" },
+                new Occupation() { Name = "Florist", Rating = "Light Manual" }
+            };
+        }
+ 
+    }
+}
---
+++ App.Repository/Interfaces/IDataRespository.cs
@@ -0,0 +1,14 @@
+﻿using App.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Repository.Interfaces
+{
+    public interface IDataRespository
+    {
+        List<Occupation> GetOccupations();
+        List<OccupationRating> GetOccupationRatings();
+        OccupationRating GetOccupationRating(string rating);
+    }
+}
