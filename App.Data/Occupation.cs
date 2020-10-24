989bfea6afe4ee2cb546575288300fcaf72c4189
avineshpatel <islandboy01@gmail.com>
2020-10-24 15:12:09 +1100

    Added the Occupation and Occupation Rating

---
+++ App.Data/Occupation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace App.Data
+{
+    public class Occupation
+    { 
+        public string Name { get; set; }
+        public string Rating { get; set; }
+    }
+}
---
+++ App.Data/OccupationRating.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace App.Data
+{
+    public class OccupationRating
+    {
+        public string Rating { get; set; }
+        public decimal Factor { get; set; }
+    }
+}
