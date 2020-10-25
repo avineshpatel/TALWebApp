96188d540a3c1fa646504fa94cd8448401122832
avineshpatel <islandboy01@gmail.com>
2020-10-25 16:26:21 +1100

    Add the api PremiumController and the premium request.

---
+++ App.Web.Api/Controllers/PremiumController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Data;
+using App.Service;
+using App.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PremiumController : ControllerBase
+    {      
+        private readonly ILogger<PremiumController> _logger;
+        private readonly IPremiumService _premiumService;
+        private readonly IOccupationService _occupationService;
+
+        public PremiumController(ILogger<PremiumController> logger, IPremiumService premiumService, IOccupationService occupationService)
+        {
+            _premiumService = premiumService;
+            _occupationService = occupationService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult GetPremium([FromBody] GetPremiumRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            decimal premium = 0;
+            OccupationRating rating = _occupationService.GetOccupationRating(request.OccupationRating);
+            
+            if (rating != null)
+            {
+                premium = _premiumService.GetPremium(request.SumInsured, rating.Factor, request.Age);
+            }
+ 
+            return Ok(premium);
+        }
+ 
+    }
+}
---
+++ App.Web.Api/Models/GetPremiumRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Web.Api.Models
+{
+    public class GetPremiumRequest
+    {
+        public int SumInsured { set; get; }
+        [Required(ErrorMessage= "OccupationRating is required")]
+        public string OccupationRating { set; get; }
+        public  int Age { set; get; }
+    }
+}
